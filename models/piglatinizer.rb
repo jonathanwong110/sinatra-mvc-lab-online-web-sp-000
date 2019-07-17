9030071a0062437f6cb6c554d61b4cf84ef9302e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-17 18:01:28 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,27 @@
+class PigLatinizer
+  
+  def piglatinize(word)
+    @word = word
+    if starts_with_a_vowel? == false
+      word.chars.rotate until starts_with_a_vowel? == true
+      end
+      word + "ay"
+    else
+      word + "way"
+    end
+  end
+
+  def to_pig_latin(sentence)
+    word = sentence.split(" ")
+  end
+  
+  
+  def starts_with_a_vowel?
+    @word.scan(/^[aeiou]/i).size > 0
+  end
+  
+end
+
+
+#word.chars.rotate.join + "ay"
+#words.chars.rotate
